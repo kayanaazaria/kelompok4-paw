5566270bc4ab510c0f5a47cedcf141182b3718b4
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-09-25 02:37:14 +0700

    finaldoc: mount /finaldoc routes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get('/', (_,res) => res.send('OK - finaldoc branch'));
+
+const finalDocRouter = require('./routes/finalDoc.routes');
+app.use('/finaldoc', finalDocRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
