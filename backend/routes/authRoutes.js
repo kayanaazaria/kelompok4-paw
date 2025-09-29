7d8c6615ac062692159cb3a5cb1127d116a0d23b
egabskr <egabnsoswet@gmail.com>
2025-09-29 17:18:32 +0700

    Fix Admin Only Register

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const { authMiddleware, roleCheck } = require('../middleware/auth');
 
-// Endpoint untuk register
-router.post('/register', registerUser);
+// Endpoint untuk register (hanya admin)
+router.post('/register', authMiddleware, roleCheck('admin'), registerUser);
 
 // Endpoint untuk login
 router.post('/login', loginUser);
