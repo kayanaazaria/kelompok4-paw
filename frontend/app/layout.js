5f4c7901142e8ffdfaefef74a602e10f89935908
Kayana Azaria <kayana.azaria@gmail.com>
2025-11-17 00:54:08 +0700

    fix: modify .gitignore, root layout.js and add favicon

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -6,17 +6,21 @@ const poppins = Poppins({
     subsets: ['latin'],
     weight: ['400', '600', '700'], 
     variable: '--font-poppins', 
+    display: 'swap',
 });
+
+export const metadata = {
+  title: 'SLI Solanum Agrotech', 
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable} font-sans`}>
       <head>
-        <title>Proyek PAW</title>
-        {}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/path-to-font.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       </head>
-      <body>
-        {}
+      <body suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
