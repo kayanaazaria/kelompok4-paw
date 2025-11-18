177f5177a2e42ce1fcde70f96c6660628b8c813f
Nicholas-Siahaan <nicholasshanepangihutansiahaan@mail.ugm.ac.id>
2025-11-18 13:07:53 +0700

    tadi nomor request / nomorLaporan tidak tersimpan di database

--- backend/models/LaporanKecelakaan.js
+++ backend/models/LaporanKecelakaan.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { DEPARTMENTS } = require('./userModel');
 
 const LaporanSchema = new mongoose.Schema({
+  nomorLaporan: { type: String, unique: true },
   tanggalKejadian: { type: Date, required: true },
   namaPekerja: { type: String, required: true },
   nomorIndukPekerja: { type: String, required: true },
