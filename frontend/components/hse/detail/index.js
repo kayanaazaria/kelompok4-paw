8c4b84c7d8769be233530c548d64714171f4167c
Tanzhydra <makhsanifatan@gmail.com>
2025-11-18 23:52:13 +0700

    Delete frontend/components directory

--- frontend/components/hse/detail/index.js
+++
@@ -1,7 +0,0 @@
-export { default as LaporanHeader } from "./LaporanHeader";
-export { default as LaporanInfo } from "./LaporanInfo";
-export { default as DetailKejadian } from "./DetailKejadian";
-export { default as LampiranSection } from "./LampiranSection";
-export { default as ApprovalInfo } from "./ApprovalInfo";
-export { default as ActionButtons } from "./ActionButtons";
-export { default as EditLaporanForm } from "./EditLaporanForm";
